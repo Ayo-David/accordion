bb12ed5010f70ffeb35d4a79d12b2c9d6c981ea2
Ayo-David Ajayi <ayodavid.ajayi@gmail.com>
2022-11-26 07:39:10 +0100

    added comments and explanations

--- src/Accordion.js
+++ src/Accordion.js
@@ -6,6 +6,13 @@ import './accordion.css'
 const Accordion = (props) => {
     //from outside, only one clicked is set for all the accordion 
     //and it changes as you click on any of the accordion
+    //to achieve this, we just use a clicked state variable that 
+    //change each time you click on any of the accordion item
+
+    //FOR MULTIPLE ACCORDION
+    //to achieve this we can use a boolean state for each accordionitem 
+    //(the boolen state can be outside or inside the accordionitem check getitdone for example).
+    //this boolean is peculiar ro each accordion item and changes per each 
     const [clicked, setClicked] = useState("0") //if useState(0) as number and not string "0", the first accordion will automatically open onload
     const handleToggle = (index) => {
         if (index === clicked) return setClicked(0)
